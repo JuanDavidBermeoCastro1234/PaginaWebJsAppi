da3392026dd5afa44e7016e170278b5f3ba1ed6a
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2025-04-08 12:53:45 -0500

    fix: :sparkles: go

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-
 const url = "https://financialmodelingprep.com/api/v3/search?query=AA&apikey=stHIvvuH0NAkvTB1RDAlLDZFwSCS8BOy";
 
+const obtenerDatos = async  () =>{
+    try{
+        const response = await fetch (url);
 
+        if(!response.ok){
+            throw new Error("Error en la respuesta de la API")
+        }
+        const data = await response.json();
+        console.log("datos recibidos", data)
+        Mostrarresultados(data);
 
-fetch (url)
-.then (response =>{
-    if (!response.ok) {
-        throw new Error("Error en la respuesta de la API");
-      }
-  return response.json();
-})
-.then(data => {
-    console.log("datos recibidos", data);
-    Mostrarresultados(data);
-})
-.catch(error => {
-    console.error("he capturado el error ")
-});
+    }catch(error){
+        console.log("error capturado", error,menssage);
+    }
+}
+
+obtenerDatos()
 
 const Mostrarresultados = (data) =>{
-    if(data.length===0){
+    if (data.length==0){
         console.log("master no hay datos")
-        return;
+        return
     }
+    
     data.forEach(item => {
         console.log(`
-        
 ------ Resultado ------
 Nombre:  ${item.name}
 Símbolo: ${item.symbol}
 Tipo:    ${item.type}
 Región:  ${item.region}
------------------------`)
+-----------------------
+        `)
+        
     });
 }
-    // Obtener el contenedor desde el HTML donde se va a insertar la tabla
-    // const contenedor = document.getElementById("contenedor")
+
+export default obtenerDatos;
