d000f885adc7b9c560e236c2e3c50ead2b52d4b5
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2025-04-07 23:34:01 -0500

    feat: :construction:

---
+++ main.js
@@ -0,0 +1,4 @@
+const url = "https://financialmodelingprep.com/stable/search-name?query=AA"
+
+    // Obtener el contenedor desde el HTML donde se va a insertar la tabla
+    const contenedor = document.getElementById("contenedor")
